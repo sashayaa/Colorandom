1615fce0cfdbee3da016abc885c2a26ef8a95b51
Aleksandra Ivanova <aleksandra98iv@gmail.com>
2023-05-30 18:09:35 +0100

    Initial setup

---
+++ src/Application.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Application extends React.Component {
+
+    render() {
+        return(
+            <div>
+                <h1>Hi</h1>
+            </div>
+        );
+    };
+}
+
+export default Application;
